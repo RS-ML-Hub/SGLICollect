5b511b457313fa157a81861d55e4c7fee1504b50
Salem <77772516+DrSalemSalem@users.noreply.github.com>
2025-09-27 23:03:00 +0100

    Create run_sglicollect.py

---
+++ run_sglicollect.py
@@ -0,0 +1,26 @@
+#
+# Copyright (c) 2023 Muhammad Salah msalah.29.10@gmail.com
+# Licensed under AGPL-3.0-or-later.
+# Refer to COPYING.txt for the AGPL license.
+# All rights reserved.
+# This project is developed as part of my research in the Remote Sensing Laboratory
+# in Kyoto University of Advanced Science towards my Master's Degree course.
+# The research was mainly supervised by Professor Salem Ibrahim Salem.
+#
+from src.args import args
+from src.download import download
+from src.search import search
+from src.extract import extract
+from src.utils import empty_temp
+
+def main() -> None:
+    if args.search:
+        search(args)
+    elif args.download:
+        download(args)
+    elif args.extract:
+        extract(args)
+    empty_temp()
+
+if __name__ == "__main__":
+    main()
